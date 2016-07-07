3fdbeed2a66ee515748d71576a7efee2d53aec7d
austingraham24 <austingraham24@gmail.com>
2016-07-06 19:17:02 -0500

    graphics difference between windows and mac
    for mac devices, the background images must be scaled up slightly

--- miners4.lua
+++ miners4.lua
@@ -52,13 +52,14 @@ function scene:create ( event )
 	local darkness = display.newImage(gameGroup,"images/darkness.png")
 	darkness.x = w/2
 	darkness.y = h/2
+	darkness:scale(1.5,1.5) -- for Mac versions only
 
 	local bg = display.newImage(gameGroup,"images/rockBackground.jpg")
 	bg.x = w/2
 	bg.y = h/2--h-bg.height/2
 	bg.rotation=180
 	bg:toBack()
-	--bg:scale(.4,.4)
+	bg:scale(1.5,1.5) -- for Mac versions only
 
 	local newRail=display.newImage(gameGroup,"images/newTrack.png")
 	newRail.x=w/2
